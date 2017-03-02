3d1c3c76815e15c6bccf5884024a5f6ad4e29dcb
Bradley J. Levine <bradlevine25@gmail.com>
2017-03-01 22:24:36 -0500

    Part of cube done

--- Assignment3/Assignment3/Game1.cs
+++ Assignment3/Assignment3/Game1.cs
@@ -11,6 +11,9 @@ namespace Assignment3
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        BasicEffect effect;
+        VertexPositionTexture[] vertexBuffer;
+        VertexPositionTexture[] bigBox;
 
         public Game1()
         {
@@ -27,6 +30,51 @@ namespace Assignment3
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            vertexBuffer = new VertexPositionTexture[8];
+            bigBox = new VertexPositionTexture[8];
+
+            //front
+            vertexBuffer[0].Position = new Vector3(50, 50, 0);  //v0
+            vertexBuffer[1].Position = new Vector3(60, 50, 0);  //v1
+            vertexBuffer[2].Position = new Vector3(50, 60, 0);  //v2
+
+            vertexBuffer[3].Position = vertexBuffer[1].Position;
+            vertexBuffer[4].Position = new Vector3(60, 60, 0);  //v3
+            vertexBuffer[5].Position = vertexBuffer[2].Position;
+
+            //back
+            vertexBuffer[6].Position = new Vector3(60, 50, 10); //v5
+            vertexBuffer[7].Position = new Vector3(50, 50, 10); //v4
+            vertexBuffer[8].Position = new Vector3(50, 60, 10); //v6
+
+            vertexBuffer[9].Position = vertexBuffer[6].Position;
+            vertexBuffer[10].Position = vertexBuffer[8].Position;
+            vertexBuffer[11].Position = new Vector3(60, 60, 10);//v7
+            
+            //right
+            vertexBuffer[12].Position = vertexBuffer[1].Position;
+            vertexBuffer[13].Position = vertexBuffer[6].Position;
+            vertexBuffer[14].Position = vertexBuffer[4].Position;
+
+            vertexBuffer[15].Position = vertexBuffer[6].Position;
+            vertexBuffer[16].Position = vertexBuffer[11].Position;
+            vertexBuffer[17].Position = vertexBuffer[4].Position;
+
+            //left
+            vertexBuffer[18].Position = vertexBuffer[7].Position;
+            vertexBuffer[19].Position = vertexBuffer[0].Position;
+            vertexBuffer[20].Position = vertexBuffer[8].Position;
+
+            vertexBuffer[21].Position = vertexBuffer[0].Position;
+            vertexBuffer[22].Position = vertexBuffer[2].Position;
+            vertexBuffer[23].Position = vertexBuffer[8].Position;
+
+            //top
+
+
+
+
 
             base.Initialize();
         }
@@ -50,6 +98,7 @@ namespace Assignment3
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
+            Content.Unload();
         }
 
         /// <summary>
@@ -65,6 +114,32 @@ namespace Assignment3
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+        }
+
+        private void DrawCube()
+        {
+            var cameraPos = new Vector3(25, 25, -10);
+            var cameraLookAtVector = Vector3.Zero;
+            var cameraUpVector = Vector3.UnitZ;
+
+            effect.View = Matrix.CreateLookAt(cameraPos, cameraLookAtVector, cameraUpVector);
+
+            float aspectRatio = graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight;
+
+            float fieldOfVeiw = MathHelper.PiOver4;
+
+            float nearClipPlane = 1f;
+            float farClipPlane = 100f;
+
+            effect.Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfVeiw, aspectRatio, nearClipPlane, farClipPlane);
+
+            foreach(var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList ,vertexBuffer, 0, 1);
+                graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertexBuffer, 1, 1);
+            }
         }
 
         /// <summary>
